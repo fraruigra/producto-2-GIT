0ca07cf053ae918a378e1ed2663547226895dc02
avillo12 <112164836+avillo12@users.noreply.github.com>
2022-10-31 16:09:36 +0100

    Update Online_Store.java

--- 2- Producto 2/grupofp/vista/Online_Store.java
+++ 2- Producto 2/grupofp/vista/Online_Store.java
@@ -10,7 +10,7 @@ public class Online_Store {
         Online_Store prg = new Online_Store();
         prg.inicio();
         // int opcion;
-    }
+  
     
     //Collections 
     Set <Articulos> articulos = new HashSet<Articulos>(); 
@@ -19,26 +19,7 @@ public class Online_Store {
     
 
     
+    }
+
     
 
-    // Escaner
-    Scanner teclado = new Scanner(System.in);
-
-    // Menu de opciones
-    void inicio() {
-
-        /*
-         * OPCIONES DE MENU
-         * 1. Anadir articulo
-         * 2. Mostrar artculo
-         * 3. Anadir clientes
-         * 4. Mostrar clientes
-         * 5. Mostrar clientes estandar
-         * 6. Mostrar clientes premium
-         * 7. Anadir pedido --> IF cliente no existe redigir a anadir cliente
-         * 8. Eliminar pedido --> SOLO si el pedido no esta enviado
-         * 9. Mostrar pedido pendientes de envio, con la opcion de filtrar por cliente
-         * 10. Mostrar pedido enviado por opcion de filtrar por cliente
-         */
-    }
-}

f4810faab663b50926c490fb82d1e4910073e2ed
avillo12 <112164836+avillo12@users.noreply.github.com>
2022-10-31 12:47:50 +0100

    Update GestionOS.java

--- 2- Producto 2/grupofp/vista/GestionOS.java
+++ 2- Producto 2/grupofp/vista/GestionOS.java
@@ -1,4 +1,7 @@
 package grupofp.vista;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 
 public class GestionOS {
     private Controlador controlador;
@@ -19,13 +22,13 @@ public class GestionOS {
             opcio = pedirOpcion();
             switch (opcio) {
                 case '1':
-                    // TO-BE-DONE
+                    Gestion_Articulos();
                     break;
                 case '2':
-                    // TO-BE-DONE
+                    Gestion_Cleintes();
                     break;
                 case '3':
-                    // TO-BE-DONE
+                    Gestion_Pedidos();
                     break;
                 case '0':
                     salir = true;
@@ -44,3 +47,96 @@ resp = " ";
 return resp.charAt(0);
 }
 }
+                  
+void Gestion_Articulos(){
+    Scanner sn = new Scanner(system.in); 
+    boolean salir = false; 
+    int option;
+    
+    while (!salir){
+        System.out.println("1. Anadir Articulo");
+        System.out.println("2. Mostrar Articulo");
+        try {
+            switch ( option) {
+                case 1:
+                    Anadir_Articulo();
+                    break;
+                case 2:
+                    Mostrar_Articulo(); 
+                    break;}
+        }
+
+}
+void Gestion_Clientes(){
+    Scanner sn = new Scanner(system.in); 
+    boolean salir = false; 
+    int option;
+    
+    while (!salir){
+        System.out.println("1. Anadir Clientes");
+        System.out.println("2. Mostrar Clientes");
+        System.out.println("3. Mostrar Clientes Estandar");
+        System.out.println("4. Mostrar Clientes Premium");
+
+        try {
+            switch ( option) {
+               case 1:
+                    Anadir_Cliente();
+                    break;
+                case 2:
+                    Mostrar_Clientes(); 
+                    break;
+                case 3:
+                    Mostrar_Cliente_Estandar(); 
+                    break;
+                 case 4:
+                    Mostrar_Cliente_VIP(); 
+                    break;}
+        }
+
+}
+void Gestion_Pedidos(){
+    Scanner sn = new Scanner(system.in); 
+    boolean salir = false; 
+    int option;
+    
+    while (!salir){
+        System.out.println("1. Anadir pedidos");
+        System.out.println("2. Mostrar pedidos");
+        System.out.println("3. Mostrar pedidos pendientes de envio");
+        System.out.println("4. Mostrar pedidos enviados");
+        try {
+            switch ( option) {
+                case 1:
+                    Anadir_Pedido();
+                    break;
+                case 2:
+                    Mostrar_Pedido(); 
+                    break;
+                case 3:
+                    Mostrar_Pedido_Pediente(); 
+                    break;
+                 case 4:
+                    Mostrar_Pedido_Enviado(); 
+                    break;}
+        }
+
+}
+    
+void Anadir_Articulo(){
+}
+void Mostrar_Articulo(){
+}
+void Anadir_Cliente(){
+}
+void Mostrar Cliente(){
+}
+void Mostrar_Cliente_Estandar(){
+}
+void Mostrar_Cliente_VIP(){
+}
+void Anadir_Pedido(){}
+void Mostrar_Pedido(){}
+void Mostrar_Pedido_Pendiente(){}
+void Mostrar_Pedido_Enviado(){}
+         
